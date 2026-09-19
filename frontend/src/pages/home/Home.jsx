0011d52da5be2a03e5c74dbd19aefd9c5d23e094
Sebastian Cruz <sebalox92@gmail.com>
2026-09-19 10:15:30 -0400

    Inicio pagina de registrar quejas

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import styles from "./home.module.css";
 
 import { useNavigate } from "react-router-dom";
 
+
 const Home = () => {
 
     const navigate = useNavigate();
@@ -14,13 +15,14 @@ const Home = () => {
 
                 <p>Tu voz importa. Registra tus quejas y haz seguimiento en tiempo real.</p>
                 <p>Centraliza la recepción de reclamos, automatiza la ivestigacion de causa raíz y gestiona planes de acción correctiva para evitar la recurrencia de fallas.</p>
+
                 <div className={styles.heroBtns}>
                     <button onClick={()=> navigate('/reportarQuejas')} className={`${styles.heroButton} ${styles.primaryButton}`}>Registrar Queja</button>
                     <button onClick={()=> navigate('/tracking')}className={`${styles.heroButton} ${styles.secondaryButton}`}>Consultar Estado</button>
-
                 </div>
-
             </div>
+
+            {/* Agregar seccion de beneficios, quizas un footer */}
         </>
     )
 }
--- frontend/src/pages/quejas/Quejas.jsx
+++ frontend/src/pages/quejas/Quejas.jsx
@@ -1,7 +1,38 @@
+import styles from "./quejas.module.css";
+import { useState } from "react";
+
 const Quejas = () => {
 
-    return(
+    return (
         <>
+            <h2 className={styles.title}>Registrar una Queja</h2>
+            <div className={styles.form}>
+                <form >
+                    <div>
+                        <input type="text" placeholder="Nombre" />
+                        <input type="email" placeholder="Correo Electronico" />
+                    </div>
+                    <div>
+                        <input type="text" placeholder="Producto Afectado" />
+                        <input type="text" placeholder="Lote / Codigo de Produccion" />
+                    </div>
+                    <div>
+                    <select name="">
+                        <option value="" selected disabled> Tipo de Queja</option>
+
+                    </select>
+
+                    <input type="file" />
+                    </div>
+                    <textarea name="">
+
+                    </textarea>
+                    <div>
+                    <button>Enviar Queja</button>
+                    </div>
+
+                </form>
+            </div>
         </>
     )
 }
