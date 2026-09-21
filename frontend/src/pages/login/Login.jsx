3f4fbff2d13dc25c498ab56b177b5c8515ed0a28
Sebastian Cruz <sebalox92@gmail.com>
2026-09-20 21:58:01 -0400

    layout y diseño de pagina de login y estados agregados al username y password

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,12 +1,33 @@
 import styles from "./login.module.css";
 import { useState } from "react";
+import logo from "../../assets/logo.png";
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleUserChange = (event) => {
+        const {value} = event.target;
+        setUsername(value);
+    }
+
+    const handlePassChange = (event) => {
+        const {value} = event.target;
+        setPassword(value);
+    }
 
     return(
         <>
             <div className={styles.container}>
                 <div className={styles.loginSection}>
+                    <img src={logo} alt="Logo" />
+                    <h3>Inicie sesión con su correo</h3>
+
+                    <form>
+                        <input type="email" placeholder="Correo Electronico" value={username} onChange={handleUserChange}/>
+                        <input type="password" placeholder="Contraseña" value={password} onChange={handlePassChange}/>
+                        <button>Inicar Sesión</button>
+                    </form>
 
                 </div>
                 <div className={styles.loginImage}></div>
