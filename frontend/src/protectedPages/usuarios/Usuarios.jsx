28ee9d8b482c6ca1f860eb80d604b28f292eb85e
Sebastian Cruz <sebalox92@gmail.com>
2026-09-22 11:12:37 -0400

    crear ruta de obtener todos los usuarios, crear tabla de vista de usuarios

--- frontend/src/protectedPages/usuarios/Usuarios.jsx
+++ frontend/src/protectedPages/usuarios/Usuarios.jsx
@@ -1,31 +1,56 @@
 import styles from "./usuarios.module.css";
+import { useEffect, useState } from "react";
+import axios from "../../axiosConfig";
 
 const Usuarios = () => {
+    const [usuarios, setUsuarios] = useState([]);
+
+    useEffect(()=> {
+
+        const getUsuarios = async () => {
+            try{
+                const response = await axios.get("http://localhost:5000/usuarios");
+
+                if (response.data.usuarios){
+                    setUsuarios(response.data.usuarios)
+                }
+
+            } catch (error) {
+                console.log("Error obteniendo usuarios.")
+            }
+        }
+        getUsuarios();
+    }, [])
+
     return (
         <>
             <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
-            <h3 className={styles.title}>Usuarios</h3>
+
+            <table className={styles.table}>
+                <thead>
+                    <th>Nombre</th>
+                    <th>Correo</th>
+                    <th>Rol</th>
+                    <th>Fecha Nacimiento</th>
+                    <th>Estado</th>
+                    <th>Acciones</th>
+                </thead>
+                <tbody>
+                    {usuarios.map(usuario=> {
+                        return(
+                            <tr>
+                                <td>{usuario.nombre} {usuario.apellido}</td>
+                                <td>{usuario.correo}</td>
+                                <td>{usuario.role}</td>
+                                <td>{usuario.fechanacimiento.split("T")[0]}</td>
+                                <td>{usuario.estado ? "Activo" : "Inactivo"}</td>
+                                <td><div className={styles.acciones}><button>Ver</button> <button>Editar</button> <button>Borrar</button></div></td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            
         </>
     )
 }
