ca69d13190c22780d873d8225fd009c4ffcd235f
Sebastian Cruz <sebalox92@gmail.com>
2026-09-24 19:14:35 -0400

    implementacion de correos API

--- backend/APIs/correos.js
+++ backend/APIs/correos.js
@@ -21,7 +21,7 @@ const enviarCorreo = async (nombre, correo, tracking) => {
 
     console.log(data); // logs response data
   } catch (error) {
-    console.log(error); //logs any error
+    console.log(error); //logs any error 
   }
 }
 
